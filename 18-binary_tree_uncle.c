303db338f3cb89ec1f63646f45e08172686dea3c
husham35 <husham35@gmail.com>
2024-03-26 18:14:44 +0000

    quick fix task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,14 +8,13 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *uncle_node;
-
-	if (!node)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	{
 		return (NULL);
-	uncle_node = node->parent->parent;
-	if (node->parent && uncle_node && uncle_node->left == node->parent)
-		return (uncle_node->right);
-	else if (node->parent && uncle_node && uncle_node->right == node->parent)
-		return (uncle_node->left);
-	return (NULL);
+	}
+	if (node->parent->parent->left == node->parent)
+	{
+		return (node->parent->parent->right);
+	}
+	return (node->parent->parent->left);
 }
